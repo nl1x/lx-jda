e0a17216d08657f7078c34b78a5651b4d9bc3456
L1x (AKA Nathan J.) <nathan.jeannot@epitech.eu>
2024-04-13 03:05:11 +0200

    feat(Main): add main class

---
+++ src/fr/nl1x/lxjda/Main.java
@@ -0,0 +1,18 @@
+package fr.nl1x.lxjda;
+
+//import fr.nl1x.lxjda.logger.Logger;
+import fr.nl1x.lxjda.logger.Loggable;
+
+public class Main implements Loggable
+{
+
+    public static void main(String[] args)
+    {
+//        try {
+//            new LxJDA();
+//        } catch (Exception err) {
+//            Logger.error(new Main(), err.getLocalizedMessage());
+//        }
+    }
+
+}
