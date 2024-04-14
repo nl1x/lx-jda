0468cb7976a2de9a5e31004edc9d51b55aef246e
L1x (AKA Nathan J.) <nathan.jeannot@epitech.eu>
2024-04-14 04:21:27 +0200

    feat(EmbedManager.java): add EmbedManager#getSuccessEmbed method and javadoc

--- src/fr/nl1x/lxjda/manager/EmbedManager.java
+++ src/fr/nl1x/lxjda/manager/EmbedManager.java
@@ -4,18 +4,33 @@ import fr.nl1x.lxjda.manager.config.BotConfig;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
+/**
+ * The EmbedManager class. It allows you to use some pre-made embeds builders.
+ */
 public class EmbedManager
 {
 
     private static EmbedManager instance;
     private final BotConfig botConfig;
 
+    /**
+     * Create a new instance of the EmbedManager class.
+     * (You are not supposed to create an instance of the embed manager,
+     * you can find the default instance in the LxJDA class.)
+     *
+     * @param botConfig The bot default configuration.
+     */
     public EmbedManager(BotConfig botConfig)
     {
         EmbedManager.instance = this;
         this.botConfig = botConfig;
     }
 
+    /**
+     * Get the default pre-made embed builder.
+     *
+     * @return the default pre-made embed builder.
+     */
     public EmbedBuilder getDefaultBuilder()
     {
         EmbedBuilder builder = new EmbedBuilder();
@@ -28,6 +43,30 @@ public class EmbedManager
         return builder;
     }
 
+    /**
+     * Get the default success embed with a custom title.
+     *
+     * @param title The title of the embed.
+     * @return the default success embed with a custom title.
+     */
+    public MessageEmbed getSuccessEmbed(String title)
+    {
+        EmbedBuilder builder = new EmbedBuilder();
+        int color = this.botConfig.getEmbedSuccessColor();
+        String icon = this.botConfig.getEmbedSuccessIcon();
+
+        builder.setAuthor(title, null, icon);
+        builder.setColor(color);
+        return builder.build();
+    }
+
+    /**
+     * Get the embed builder of the pre-made error embed.
+     *
+     * @param title The title of the pre-made error embed.
+     * @param error The error message that will be shown in the description of the embed.
+     * @return the embed builder of the pre-made error embed.
+     */
     public EmbedBuilder getErrorBuilder(String title, String error)
     {
         EmbedBuilder builder = this.getDefaultBuilder();
@@ -40,16 +79,33 @@ public class EmbedManager
         return builder;
     }
 
+    /**
+     * Get the embed builder of the pre-made error embed.
+     *
+     * @param error The error message that will be shown in the description of the embed.
+     * @return the embed builder of the pre-made error embed.
+     */
     public EmbedBuilder getErrorBuilder(String error)
     {
         return this.getErrorBuilder(this.botConfig.getEmbedErrorTitle(), error);
     }
 
+    /**
+     * Get a pre-made embed error with a custom message.
+     *
+     * @param error The error message to put in the embed description.
+     * @return the fully prepared error embed.
+     */
     public MessageEmbed getError(String error)
     {
         return this.getErrorBuilder(error).build();
     }
 
+    /**
+     * Get the last created instance of the embed manager class.
+     *
+     * @return the last created instance of the embed manager class.
+     */
     public static EmbedManager getInstance()
     {
         return EmbedManager.instance;
