dbe302df6584d0dd2226cdf26ad2abbb69c88e44
L1x (AKA Nathan J.) <nathan.jeannot@epitech.eu>
2024-04-14 04:14:41 +0200

    feat(LxJDA.java): add custom Gateway Intents, abstract method registerConfigs, slash commands support

--- src/fr/nl1x/lxjda/LxJDA.java
+++ src/fr/nl1x/lxjda/LxJDA.java
@@ -1,5 +1,6 @@
 package fr.nl1x.lxjda;
 
+import fr.nl1x.lxjda.listeners.OnCommand;
 import fr.nl1x.lxjda.logger.Loggable;
 import fr.nl1x.lxjda.manager.ConfigManager;
 import fr.nl1x.lxjda.manager.EmbedManager;
@@ -8,9 +9,13 @@ import fr.nl1x.lxjda.manager.CommandManager;
 import fr.nl1x.lxjda.logger.Logger;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.io.IOException;
 
+/**
+ * The {@code LxJDA} class allow you to easily create and manage your discord bot.
+ */
 public abstract class LxJDA implements Loggable
 {
 
@@ -23,7 +28,13 @@ public abstract class LxJDA implements Loggable
     private final JDABuilder builder;
     private JDA jda;
 
-    public LxJDA() throws IOException
+    /**
+     * Create an instance of LxJDA.
+     * (It is better to extend from this class than to create an instance of this class.)
+     *
+     * @throws IOException if the configuration manager failed to load the default bot configuration file.
+     */
+    public LxJDA(GatewayIntent intent, GatewayIntent... intents) throws IOException
     {
         LxJDA.instance = this;
 
@@ -33,6 +44,7 @@ public abstract class LxJDA implements Loggable
         embedManager = new EmbedManager(configManager.getBotConfig());
 
         this.builder = JDABuilder.createDefault(configManager.getBotConfig().getToken());
+        this.builder.enableIntents(intent, intents);
     }
 
     /**
@@ -45,7 +57,11 @@ public abstract class LxJDA implements Loggable
         Logger.info("Starting bot...");
         jda = this.builder.build().awaitReady();
 
+        Logger.info("Registering configuration files...");
+        this.registerConfigs();
+
         Logger.info("Registering events...");
+        this.getEventManager().registerEvents(new OnCommand(this));
         this.registerEvents();
 
         Logger.info("Registering commands...");
@@ -57,40 +73,84 @@ public abstract class LxJDA implements Loggable
         Logger.info("Bot ready to use!");
     }
 
+    /**
+     * Get the last created instance of LxJDA.
+     *
+     * @return the last created instance of LxJDA.
+     */
     public static LxJDA getInstance()
     {
         return LxJDA.instance;
     }
 
+    /**
+     * Get the bot JDA.
+     *
+     * @return the bot JDA.
+     */
     public static JDA getJDA()
     {
         return LxJDA.getInstance().jda;
     }
 
+    /**
+     * Get the command manager.
+     *
+     * @return the command manager.
+     */
     public CommandManager getCommandManager()
     {
         return this.commandManager;
     }
 
+    /**
+     * Get the configuration manager.
+     *
+     * @return the configuration manager.
+     */
     public ConfigManager getConfigManager()
     {
         return this.configManager;
     }
 
+    /**
+     * Get the embed manager.
+     *
+     * @return the embed manager.
+     */
     public EmbedManager getEmbedManager()
     {
         return this.embedManager;
     }
 
+    /**
+     * Get the event manager.
+     *
+     * @return the event manager.
+     */
     public EventManager getEventManager()
     {
         return eventManager;
     }
 
+    /**
+     * Register all the events that the bot will listen to.
+     */
     public abstract void registerEvents();
 
+    /**
+     * Register all the commands. (A command is an instance of a class that extends from CommandExecutor)
+     */
     public abstract void registerCommands();
 
+    /**
+     * Register all the buttons that the bot will handle. (Currently not available.)
+     */
     public abstract void registerButtons();
+
+    /**
+     * Register all the configuration files that the bot will use.
+     */
+    public abstract void registerConfigs();
 
 }
